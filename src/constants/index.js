e437740c7c09009efb8309974a6275a5c98304f1
Jake Hayes <jayhay28324@gmail.com>
2020-03-09 07:10:41 -0400

    Updated personal info

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,29 +1,26 @@
 exports.CONTENT_PER_PAGE = 8;
 exports.PAGE_PER_SCREEN = 4;
 
-exports.POST = 'post';
-exports.PORTFOLIO = 'portfolio';
-exports.RESUME = 'resume';
+exports.POST = "post";
+exports.PORTFOLIO = "portfolio";
+exports.RESUME = "resume";
 
 // Site Information
-exports.NAME = 'Gatsby Advanced Blog';
-exports.TITLE = 'Gatsby Advanced Blog';
-exports.PREFIX = 'WONISM | ';
-exports.DESCRIPTION = 'Create your own blog with Gatsby Advanced Blog';
-exports.AUTHOR = 'wonism';
-exports.EMAIL = 'yocee57@gmail.com';
-exports.KEYWORDS = [
-  'Gatsby',
-  'ReactJS',
-].join(', ');
-exports.SITE_URL = 'https://wonism.github.io';
-exports.PROFILE = 'https://avatars3.githubusercontent.com/u/42737121?s=200&v=4';
+exports.NAME = "Jake Hayes";
+exports.TITLE = "Jake Hayes";
+exports.PREFIX = "Jake Hayes | ";
+exports.DESCRIPTION = "Jake Hayes' personal blog";
+exports.AUTHOR = "Jake Hayes";
+exports.EMAIL = "jakejhayes@gmail.com";
+exports.KEYWORDS = ["Gatsby", "ReactJS"].join(", ");
+exports.SITE_URL = "https://jakehayes.net";
+exports.PROFILE = "https://avatars3.githubusercontent.com/u/42737121?s=200&v=4";
 
 // Social
-exports.GITHUB_ID = 'wonism';
-exports.TWITTER_ID = 'j1ism';
-exports.FACEBOOK_ID = 'j1.chic';
-exports.LINKEDIN_ID = 'wonism';
+exports.GITHUB_ID = "thejayhaykid";
+exports.TWITTER_ID = "thejayhaykid";
+exports.FACEBOOK_ID = "thejayhaykid";
+exports.LINKEDIN_ID = "thejayhaykid";
 
 // Disqus (Recommend you to use this)
-exports.DISQUS_ID = 'sample';
+exports.DISQUS_ID = "thejayhaykid";
