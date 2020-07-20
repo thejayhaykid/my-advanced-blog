432167e8cc56e19b16093440efea253eeb49f0cc
Jake Hayes <JayHay28324@gmail.com>
2020-07-20 18:58:39 -0400

    WIP - Change modal to close when clicking out of it

--- src/components/Common/ImageModal/index.js
+++ src/components/Common/ImageModal/index.js
@@ -1,33 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import styles from "./ImageModal.module.scss";
-import closeX from "../../../resources/close_x.svg";
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+function useOutsideAlerter(ref) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        console.log("click");
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
 
 const ImageModal = ({ showModal, setShowModal, imageURL, setImageURL }) => {
   const modalClose = () => {
     setShowModal(false);
     setImageURL("");
   };
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
 
   return (
     <>
       {showModal ? (
         <div className={styles.ModalDiv}>
-          <div className={styles.mainModal}>
-            {/* <div className={styles.imageDiv}> */}
-            {/* <img
-              className={styles.closeX}
-              onClick={modalClose}
-              src={closeX}
-              alt="Close image"
-            /> */}
+          <div className={styles.mainModal} ref={wrapperRef}>
             <img
               className={styles.mainImg}
               onClick={modalClose}
               src={imageURL}
             />
           </div>
-          {/* </div> */}
         </div>
       ) : null}
     </>
